740912339b4d6bd6f4bf58d8f26ac79bf93d0b07
Anush <anush.formal@outlook.com>
2024-02-24 17:02:46 +0000

    added Likes, Bookmars and Posts table with migrations

--- database/migrations/2024_02_24_170027_create_bookmarks_table.php
+++ database/migrations/2024_02_24_170027_create_bookmarks_table.php
@@ -11,14 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('bookmarks', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->string('about_me')->nullable();
-            $table->rememberToken();
+            $table->foreignId('user_id')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreignId('post_id')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
             $table->timestamps();
         });
     }
@@ -28,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('bookmarks');
     }
 };
